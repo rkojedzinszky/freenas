681eb838624bfb710168b74d9808714c7e3d22c3
Richard Kojedzinszky <krichy@nmdps.net>
2018-07-18 14:32:27 +0200

    iSCSI: not offering snapshots

--- src/middlewared/middlewared/plugins/iscsi.py
+++ src/middlewared/middlewared/plugins/iscsi.py
@@ -694,9 +694,6 @@ class iSCSITargetExtentService(CRUDService):
         async for pdisk in await self.middleware.call('pool.get_disks'):
             used_disks.append(pdisk)
 
-        zfs_snaps = await self.middleware.call('zfs.snapshot.query', [],
-                                               {'order_by': ['name']})
-
         zvols = await self.middleware.call(
             'pool.dataset.query',
             [('type', '=', 'VOLUME')]
@@ -709,16 +706,6 @@ class iSCSITargetExtentService(CRUDService):
             zvol_size = zvol['volsize']['value']
             if f'zvol/{zvol_name}' not in used_zvols:
                 diskchoices[f'zvol/{zvol_name}'] = f'{zvol_name} ({zvol_size})'
-
-        for snap in zfs_snaps:
-            ds_name, snap_name = snap['properties']['name'][
-                'value'].rsplit('@', 1)
-            full_name = f'{ds_name}@{snap_name}'
-            snap_size = snap['properties']['referenced']['value']
-
-            if ds_name in zvol_list:
-                diskchoices[f'zvol/{full_name}'] = \
-                    f'{full_name} ({snap_size}) [ro]'
 
         notifier_disks = await self.middleware.call('notifier.get_disks')
         for name, disk in notifier_disks.items():
